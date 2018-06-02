825ee5536b85de75435b97a753a209e7786eaa3f
Daniel Jabry <daniel.jabry@rezonence.com>
2018-06-02 23:21:58 +0100

    Upgraded dependencies

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -2,93 +2,93 @@
 // Generated on Fri Nov 29 2013 12:39:24 GMT+0000 (GMT)
 
 module.exports = function(config) {
-  config.set({
+    config.set({
 
-    // base path, that will be used to resolve files and exclude
-    basePath: '../',
-
-
-    // frameworks to use
-    frameworks: ['mocha', 'browserify'],
+        // base path, that will be used to resolve files and exclude
+        basePath: '../',
 
 
-    // list of files / patterns to load in the browser
-    files: [
-      { pattern: 'test/*.js', included: true },
-
-      // code
-      { pattern: 'lib/*.js', watched: true, served: false, included: false },
-      { pattern: 'index.js', watched: true, served: false, included: false },
-
-      // fixtures
-      { pattern: 'samples/*', watched: true, served: true, included: false },
-    ],
-
-    preprocessors: {
-      '**/*.coffee': ['coffee'],
-      'test/*.js': ['browserify'],
-    },
-
-    // list of files to exclude
-    exclude: [
-      '**/.*.swp',
-      '**/*~'
-    ],
+        // frameworks to use
+        frameworks: ['mocha', 'browserify'],
 
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
-    reporters: ['progress'],
+        // list of files / patterns to load in the browser
+        files: [
+            { pattern: 'test/*.js', included: true },
+
+            // code
+            { pattern: 'lib/*.js', watched: true, served: false, included: false },
+            { pattern: 'index.js', watched: true, served: false, included: false },
+
+            // fixtures
+            { pattern: 'samples/*', watched: true, served: true, included: false },
+        ],
+
+        preprocessors: {
+            '**/*.coffee': ['coffee'],
+            'test/*.js': ['browserify'],
+        },
+
+        // list of files to exclude
+        exclude: [
+            '**/.*.swp',
+            '**/*~'
+        ],
 
 
-    // web server port
-    port: 9876,
+        // test results reporter to use
+        // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+        reporters: ['progress'],
 
 
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
+        // web server port
+        port: 9876,
 
 
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
 
 
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
-
-    // Start these browsers, currently available:
-    // - Chrome
-    // - ChromeCanary
-    // - Firefox
-    // - Opera (has to be installed with `npm install karma-opera-launcher`)
-    // - Safari (only Mac; has to be installed with `npm install karma-safari-launcher`)
-    // - PhantomJS
-    // - IE (only Windows; has to be installed with `npm install karma-ie-launcher`)
-    browsers: ['Chrome', 'Firefox'],
+        // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: config.LOG_INFO,
 
 
-    // If browser does not capture in given timeout [ms], kill it
-    captureTimeout: 60000,
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: true,
+
+        // Start these browsers, currently available:
+        // - Chrome
+        // - ChromeCanary
+        // - Firefox
+        // - Opera (has to be installed with `npm install karma-opera-launcher`)
+        // - Safari (only Mac; has to be installed with `npm install karma-safari-launcher`)
+        // - PhantomJS
+        // - IE (only Windows; has to be installed with `npm install karma-ie-launcher`)
+        browsers: ['Chrome', 'Firefox'],
 
 
-    // Continuous Integration mode
-    // if true, it capture browsers, run tests and exit
-    singleRun: false,
+        // If browser does not capture in given timeout [ms], kill it
+        captureTimeout: 60000,
 
-    browserify: {
-      extension: ['.coffee'],
-      transform: [],
-      // require: ['native-buffer-browserify'],
-      watch: true
-    },
 
-    plugins: [
-      'karma-chrome-launcher',
-      'karma-firefox-launcher',
-      'karma-phantomjs-launcher',
-      'karma-browserify',
-      'karma-mocha'
-    ]
-  });
+        // Continuous Integration mode
+        // if true, it capture browsers, run tests and exit
+        singleRun: false,
+
+        browserify: {
+            extension: ['.coffee'],
+            transform: [],
+            // require: ['native-buffer-browserify'],
+            watch: true
+        },
+
+        plugins: [
+            'karma-coffee-preprocessor',
+            'karma-chrome-launcher',
+            'karma-firefox-launcher',
+            'karma-browserify',
+            'karma-mocha'
+        ]
+    });
 };
--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-var gribParse = require('./lib/parser');
-var tables = require('./lib/tables');
-var jBinary = require('jbinary');
+const gribParse = require('./lib/parser');
+const tables = require('./lib/tables');
+const jBinary = require('jbinary');
 
-for(var tableName in tables.tables) {
+for(const tableName in tables.tables) {
   exports[tableName] = tables.tables[tableName];
 }
 
 exports.readData = function(data, cb) {
-  var msgs;
-  var dataView = new jBinary(data).view;
+    let msgs;
+    const dataView = new jBinary(data).view;
 
-  // Write the contents of the buffer catching any parse errors
+    // Write the contents of the buffer catching any parse errors
   try {
     msgs = gribParse.parseDataView(dataView);
   } catch(e) {
@@ -18,14 +18,14 @@ exports.readData = function(data, cb) {
   }
 
   // If no messages were parsed throw an error
-  if(msgs.length == 0) { return cb(new Error('No GRIB messages could be decoded')); }
+  if(msgs.length === 0) { return cb(new Error('No GRIB messages could be decoded')); }
 
   cb(null, msgs);
-}
+};
 
 exports.readUrl = function(url, cb) {
   jBinary.loadData(url, function(err, data) {
     if(err) return cb(err);
     exports.readData(data, cb);
   });
-}
+};
